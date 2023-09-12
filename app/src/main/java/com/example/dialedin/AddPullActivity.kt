0128589742643a471e4d4465baee70c7350edfc2
Alex <alexprosd@gmail.com>
2023-09-12 15:04:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dialedin/AddPullActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dialedin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPullActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_pull_activity)
+    }
+}
