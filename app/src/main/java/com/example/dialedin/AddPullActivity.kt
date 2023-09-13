340cad8f42cf0f1d4b9c8e12a87de31950178b6a
Alex <alexprosd@gmail.com>
2023-09-12 19:22:25 -0700

    Progress on AddPullActivity, start of MainActivity

--- app/src/main/java/com/example/dialedin/AddPullActivity.kt
+++ app/src/main/java/com/example/dialedin/AddPullActivity.kt
@@ -2,10 +2,32 @@ package com.example.dialedin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toolbar
 
 class AddPullActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_pull_activity)
+
+        //addPullToolbar functionality
+        val addPullToolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.addPullToolbar)
+        setSupportActionBar(addPullToolbar)
+
+        //ratingSeekbar functionality
+        val ratingSeekBar: SeekBar = findViewById(R.id.ratingSeekBar)
+        var ratingDisplay: TextView = findViewById(R.id.ratingDisplay)
+        ratingSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                ratingDisplay.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+
     }
 }
